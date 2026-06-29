09b4c2a828d0b82ecbf0d8262effcac2385012ae
Beto Kosinski <beto.kosinski2@gmail.com>
2026-06-28 23:14:50 -0300

    Implemented Basic Movement and Camera Logic

---
+++ Source/EndlessRunner/RunnerCharacter.cpp
@@ -0,0 +1,91 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RunnerCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+ARunnerCharacter::ARunnerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_GameTraceChannel1, ECR_Overlap);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	SideViewCamera = CreateDefaultSubobject<UCameraComponent>(FName("SideViewCamera"));
+	SideViewCamera->bUsePawnControlRotation	= false;	
+		
+	//Remove this later for future game design choice
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
+	
+	GetCharacterMovement()->GravityScale = 2.0f;
+	GetCharacterMovement()->AirControl = 0.8f;
+	GetCharacterMovement()->JumpZVelocity = 1000.0f;
+	GetCharacterMovement()->GroundFriction = 3.0f;
+	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
+	GetCharacterMovement()->MaxFlySpeed = 600.0f;
+
+	tempPos = GetActorLocation();
+	zPosition = tempPos.Z + 300.0f;
+	
+}
+
+// Called when the game starts or when spawned
+void ARunnerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CanMove = true;
+	
+}
+
+// Called every frame
+void ARunnerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	tempPos = GetActorLocation();
+	tempPos.X -= 850.0f;
+	tempPos.Z = zPosition;
+	SideViewCamera->SetWorldLocation(tempPos);		
+
+}
+
+// Called to bind functionality to input
+void ARunnerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ARunnerCharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ARunnerCharacter::StopJumping);
+	
+	PlayerInputComponent->BindAxis("MoveRight", this, &ARunnerCharacter::MoveRight);
+}
+
+void ARunnerCharacter::MoveRight(float Value)
+{
+	if(CanMove)
+	{
+		AddMovementInput(FVector(0.0f, 1.0f, 0.0f), Value);
+	}
+}
+
+void ARunnerCharacter::RespawnCharacter()
+{
+	
+}
+
+void ARunnerCharacter::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
+
